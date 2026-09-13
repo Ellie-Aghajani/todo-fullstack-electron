d9acfc11277a7cb661ee286f065194f402c56339
Ellie-Aghajani <z.aqajani@gmail.com>
2026-09-12 20:16:34 -0700

    fix: add CORS policy to allow requests from Vite dev server

--- backend/TodoApi/Program.cs
+++ backend/TodoApi/Program.cs
@@ -5,17 +5,28 @@ using TodoApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontendDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:5174")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
 builder.Services.AddControllers();
-
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<TodoActivityLogger>();
 builder.Services.AddScoped<RequestAuditor>();
+
 var app = builder.Build();
+app.UseCors("AllowFrontendDev");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
