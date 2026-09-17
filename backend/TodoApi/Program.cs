4daaaf67bd5c3df56c8b37c247692cb2c81c2479
Ellie-Aghajani <z.aqajani@gmail.com>
2026-09-17 15:11:47 -0700

    feat: add EnableRetryOnFailure for Azure SQL transient fault resilience

--- backend/TodoApi/Program.cs
+++ backend/TodoApi/Program.cs
@@ -27,8 +27,14 @@ builder.Services.AddCors(options =>
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null
+        )
+    ));
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
